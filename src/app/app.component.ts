bed467a7a5ed7ff98b0b12a361e198082cd9dde8
rankesh2004 <rangesh.csr.2004@gmail.com>
2024-09-14 17:31:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'train';
-}
+export class AppComponent implements OnInit{
+  httpClient = inject(HttpClient)
+  cryptoData:any
+
+  ngOnInit(): void {
+    this.fetchData()
+  }
+
+  fetchData():void{
+    this.httpClient.get("https://apis.ccbp.in/crypto-currency-converter").subscribe((data)=>{
+      console.log(data)
+      this.cryptoData = data
+    })
+  }
+}   
